b1663bf5612178ee65b43f3ef82c696986ebe5a1
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2023-05-04 23:36:33 +0800

    Update update.sh

--- UPDATE/update.sh
+++ UPDATE/update.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
 #Multiport Fallbacks By Vinstechmy
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+
+# Github Profile Repo
+Git_Profile="https://raw.githubusercontent.com/vinstechmy/NginxFallbackMultiport/main"
+
+echo -e "[ ${green}INFO${NC} ] Update Starting Now . . ."
+echo ""
+sleep 1
+cd /usr/bin
+rm get-backres
+rm backupmenu
+
+wget -O get-backres "${Git_Profile}/OTHERS/get-backres.sh" && chmod +x /usr/bin/get-backres
+wget -O backupmenu "${Git_Profile}/MENU/backupmenu.sh" && chmod +x /usr/bin/backupmenu
+cd
+cd /etc/nginx/conf.d
+rm xray.conf
+wget -O xray.conf "https://raw.githubusercontent.com/vinstechmy/NginxFallbackMultiport/main/OTHERS/xray.conf" && chmod +x xray.conf
+sleep 1
+clear
+echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+sleep 1
+echo -e "[${green}INFO${NC}] Please Restart All Services !"
+sleep 1
+clear
+menu
